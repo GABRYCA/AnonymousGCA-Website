1fd6ba03717cfa88b0b82fea29df66aae093830f
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2023-11-25 11:59:38 +0100

    Changelogs: - Just a single comment.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-node'; // Depending on deployment target, you may need to change this another adapter
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
