e1fe1fc6f98444acacd9d877dd844ce1d2ae5834
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2023-12-27 23:31:14 +0100

    Changelogs: - New collection page - Update website font - Other general changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,6 +27,9 @@
             window.AOS.refresh();
         }
     }
+
+    // Automatically update footer year
+    const currentYear = new Date().getFullYear();
 </script>
 
 <svelte:head>
@@ -51,8 +54,11 @@
                 <li class="nav-item">
                     <a class="nav-link" href="contacts"><i class="fas fa-address-book"></i> Contacts</a>
                 </li>
-                <li class="nav-item">
+                <!--<li class="nav-item">
                     <a class="nav-link" href="passive_income"><i class="fas fa-money-bill-wave"></i> Passive Income</a>
+                </li>-->
+                <li class="nav-item">
+                    <a class="nav-link" href="collection"><i class="fa-brands fa-raspberry-pi"></i> Pi</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="https://tales.anonymousgca.eu/" target="_blank" data-bs-toggle="tooltip" data-bs-placement="bottom" title="External Link"><i class="fas fa-external-link-alt"></i> Tales</a>
@@ -106,12 +112,18 @@
     <SVGWave data="M0,174L48,174C96,174,192,174,288,183.7C384,193,480,213,576,188.5C672,164,768,97,864,96.7C960,97,1056,164,1152,198.2C1248,232,1344,232,1440,232C1536,232,1632,232,1728,227.2C1824,222,1920,213,2016,212.7C2112,213,2208,222,2304,222.3C2400,222,2496,213,2592,188.5C2688,164,2784,126,2880,125.7C2976,126,3072,164,3168,188.5C3264,213,3360,222,3456,198.2C3552,174,3648,116,3744,91.8C3840,68,3936,77,4032,101.5C4128,126,4224,164,4320,174C4416,184,4512,164,4608,154.7C4704,145,4800,145,4896,140.2C4992,135,5088,126,5184,140.2C5280,155,5376,193,5472,183.7C5568,174,5664,116,5760,120.8C5856,126,5952,193,6048,227.2C6144,261,6240,261,6336,251.3C6432,242,6528,222,6624,188.5C6720,155,6816,106,6864,82.2L6912,58L6912,290L6864,290C6816,290,6720,290,6624,290C6528,290,6432,290,6336,290C6240,290,6144,290,6048,290C5952,290,5856,290,5760,290C5664,290,5568,290,5472,290C5376,290,5280,290,5184,290C5088,290,4992,290,4896,290C4800,290,4704,290,4608,290C4512,290,4416,290,4320,290C4224,290,4128,290,4032,290C3936,290,3840,290,3744,290C3648,290,3552,290,3456,290C3360,290,3264,290,3168,290C3072,290,2976,290,2880,290C2784,290,2688,290,2592,290C2496,290,2400,290,2304,290C2208,290,2112,290,2016,290C1920,290,1824,290,1728,290C1632,290,1536,290,1440,290C1344,290,1248,290,1152,290C1056,290,960,290,864,290C768,290,672,290,576,290C480,290,384,290,288,290C192,290,96,290,48,290L0,290Z" />
     <div class="row text-center bg-black bg-opacity-10 pb-3">
         <div class="col">
-            <p class="h6">© 2023 AnonymousGCA</p>
+            <p class="h6">© {currentYear} AnonymousGCA</p>
         </div>
     </div>
 </footer>
 
 <style>
+
+    /* Global font for all kind of texts on all pages (must be in row to work, just a workaround) */
+    :global(.row){
+        font-family: 'Roboto', sans-serif !important;
+    }
+
     .navbar-brand {
         text-shadow: 2px 2px #000;
     }
---
+++ src/routes/collection/+page.svelte
@@ -0,0 +1,207 @@
+<script>
+    import ozerofront from"$lib/images/o-zero2w-front.png";
+    import ozeroangle from "$lib/images/o-zero2w-angled.png";
+    import orangelogo from "$lib/images/orange-logo.png";
+    import {onMount} from "svelte";
+
+    onMount(() => {
+        window.$('[data-bs-toggle="tooltip"]').tooltip();
+    });
+</script>
+
+<svelte:head>
+    <title>Collection - AnonymousGCA</title>
+    <meta name="description" content="AnonymousGCA's contacts">
+</svelte:head>
+
+<div class="container">
+    <div class="row justify-content-center mt-4">
+        <div class="col text-center">
+            <p class="h1 title">The small collection:</p>
+        </div>
+    </div>
+
+    <div class="row mt-4">
+
+        <!-- Raspberry Pi Zero 2W Info-->
+        <div class="col-12 col-md-7 bg-black bg-opacity-50 p-4 pb-2 rounded-5 raspberry">
+            <div class="row">
+                <div class="col-12">
+                    <p class="h4"><i class="fa-brands fa-raspberry-pi"></i> Raspberry Pi Zero 2W</p>
+                    <p class="text-start mb-1">Deployment: <span class="text-primary-emphasis">25/12/2021</span> - Status: <span class="text-warning-emphasis">Retired</span></p>
+                    <p class="text-start text-light opacity-50">[Replaced by Orange Pi]</p>
+                    <hr>
+                    <p>Always loved <b>Raspberry Pi</b> as an efficient, low power, little Linux Server. However, this one
+                    specifically comes with some limitations:</p>
+                    <ul>
+                        <li class="text-danger">512MB RAM</li>
+                        <li class="text-success-emphasis">4-Core 64Bit ARM CPU</li>
+                        <li class="text-warning-emphasis">Slow 2.4GHz WiFi</li>
+                        <li class="text-warning-emphasis">Bluetooth 4.2</li>
+                        <li>1x Micro-USB 2.0</li>
+                        <li>1x Mini HDMI</li>
+                        <li>1x Power Micro USB</li>
+                        <li>1x CSI Camera Connector</li>
+                        <li>1x GPIO 40-pin</li>
+                    </ul>
+                    <p>Despite its very limited amount of RAM and the very slow WiFi (less than 40mbps), it served me well for many years,
+                    even if with some hiccups:</p>
+                    <ul>
+                        <li class="text-success-emphasis">Terminal-Only, no desktop for my usage</li>
+                        <li class="text-warning-emphasis">Always Swapping <span class="text-danger">(killed a 128GB Samsung EVO Plus Micro SD, luckily under warranty)</span></li>
+                        <li class="text-warning-emphasis">Slow WiFi</li>
+                        <li class="text-warning">Kept freezing and required a reboot (Pull the plug)</li>
+                    </ul>
+                    <hr>
+                    <p class="h5">Usage:</p>
+                    <ul>
+                        <li>Docker Containers</li>
+                        <li>Background Tasks (Passive Income)</li>
+                        <li>Many experiments and emulators (Even learned about binfmt, and could even run AMD64 programs thanks to that!)</li>
+                        <li>Little Webserver (APACHE)</li>
+                    </ul>
+                    <hr>
+                    <p class="h5">Retired:</p>
+                    <ul>
+                        <li>December 2023</li>
+                    </ul>
+                    <hr>
+                    <p class="h5">Replaced by:</p>
+                    <ul>
+                        <li>Orange Pi Zero 2W 4GB</li>
+                    </ul>
+                    <hr>
+                    <p class="h5">Price (Time of buy):</p>
+                    <ul>
+                        <li class="text-success-emphasis">23€ including shipping fees</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- Raspberry Pi Zero 2W Images-->
+        <div class="col-12 col-md-5 pt-5">
+            <p class="text-center mt-md-5 opacity-25"><i class="fa-brands fa-raspberry-pi fa-10x text-light"></i></p>
+            <img src="https://assets.raspberrypi.com/static/51035ec4c2f8f630b3d26c32e90c93f1/2b8d7/zero2-hero.webp" class="img-fluid rounded-5 mt-md-5 mx-auto d-block" alt="Raspberry Pi Zero 2W">
+            <img src="https://assets.raspberrypi.com/static/f03a00485ac096c1033ad1c4a530d63b/6fe49/zero2-close-up.webp" class="img-fluid rounded-5 mt-md-5 mx-auto d-block" alt="Raspberry Pi Zero 2W">
+        </div>
+
+        <div class="col-12 mt-5 mb-5">
+            <hr>
+        </div>
+
+        <!-- Orange pi zero 2W Images -->
+        <div class="col-12 col-md-5 pt-5 pb-4">
+            <img src="{orangelogo}" class="img-fluid rounded-5 mt-md-5 mx-auto d-block" alt="Orange Pi Zero 2W">
+            <img src="{ozeroangle}" class="img-fluid rounded-5 mt-3 mt-md-5 mx-auto d-block" alt="Raspberry Pi Zero 2W">
+            <img src="{ozerofront}" class="img-fluid rounded-5 mt-4 mt-md-5 mx-auto d-block" alt="Raspberry Pi Zero 2W">
+        </div>
+        <!-- Orange Pi Zero 2W Info -->
+        <div class="col-12 col-md-7 bg-black bg-opacity-50 p-4 pb-2 rounded-5 orange">
+            <div class="row">
+                <div class="col-12">
+                    <p class="h4"> Orange Pi Zero 2W 4GB</p>
+                    <p class="text-start mb-1">Deployment: <span class="text-primary-emphasis">01/12/2023</span> - Status: <span class="text-success-emphasis">Operational</span></p>
+                    <hr>
+                    <p>Intrigued by the specs and deal (28$) this alternative Raspberry Pi Zero 2W like clone got into my hands. Since then, I loved it!</p>
+                    <ul>
+                        <li class="text-success-emphasis">4GB RAM</li>
+                        <li class="text-success-emphasis">4-Core 64Bit ARM CPU</li>
+                        <li class="text-success-emphasis">2.4/5GHz WiFi 5</li>
+                        <li class="text-success-emphasis">Bluetooth 5.0 BLE Support</li>
+                        <li>1x USB-C 2.0</li>
+                        <li>1x Mini HDMI</li>
+                        <li>1x Power USB-C</li>
+                        <li>1x CSI Camera Connector</li>
+                        <li>1x GPIO 40-pin</li>
+                    </ul>
+                    <p>Despite its small price and sizes, those 4GB of LPDDR4 RAM are great, and the WiFi module, handles easily 100mbps.
+                    However, it had some problems initially:</p>
+                    <ul>
+                        <li class="text-warning-emphasis">Not clear enough initial connection setup (All guides mentioned to use an ethernet
+                        dongle, which I didn't have, but in the Ubuntu ISO image there was an hidden first_run config file which allowed after editing to enable and connect to WiFi
+                        automatically</li>
+                        <li class="text-warning-emphasis">Default <b><u>apt</u></b> Huawei Repositories (Thankfully you can change them through Linux configs back to ports.ubuntu.com and it works just fine)</li>
+                    </ul>
+                    <hr>
+                    <p class="h5">Usage:</p>
+                    <ul>
+                        <li>Even more Docker Containers</li>
+                        <li>Background Tasks (Passive Income)</li>
+                        <li>Many experiments and emulators (Still using binfmt, and can run AMD64 programs thanks that)</li>
+                        <li>Little Webserver (APACHE)</li>
+                    </ul>
+                    <hr>
+                    <p class="h5">Price (Time of buy):</p>
+                    <ul>
+                        <li class="text-success-emphasis">About 27€ including shipping fees after change </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+<style>
+
+    .h1.title {
+        color: #ffffff;
+        text-align: center;
+        font-size: 2.5em;
+        text-shadow: 0 0 5px #29ff10, 0 0 25px #29ff10, 0 0 50px #29ff10, 0 0 200px #29ff10;
+        animation: glowing 2s infinite;
+    }
+
+    @keyframes glowing {
+        0% { text-shadow: 0 0 5px #29ff10, 0 0 25px #29ff10, 0 0 50px #29ff10, 0 0 200px #29ff10; }
+        50% { text-shadow: 0 0 10px #29ff10, 0 0 20px #29ff10, 0 0 40px #29ff10, 0 0 80px #29ff10; }
+        100% { text-shadow: 0 0 5px #29ff10, 0 0 25px #29ff10, 0 0 50px #29ff10, 0 0 200px #29ff10; }
+    }
+
+    /* Raspberry Pi */
+    .bg-black.bg-opacity-50.p-4.pb-2.rounded-5.raspberry {
+        position: relative;
+        overflow: hidden;
+    }
+
+    .bg-black.bg-opacity-50.p-4.pb-2.rounded-5.raspberry::before,
+    .bg-black.bg-opacity-50.p-4.pb-2.rounded-5.raspberry::after {
+        content: "";
+        position: absolute;
+        top: -2px;
+        left: -2px;
+        width: calc(100% + 4px);
+        height: calc(100% + 4px);
+        background: linear-gradient(120deg, #000000, rgba(199, 5, 61, 0.25), #000000);
+        z-index: -1;
+        background-size: 300% 300%;
+        animation: wave 25s linear infinite;
+    }
+
+    /* Orange Pi */
+    .bg-black.bg-opacity-50.p-4.pb-2.rounded-5.orange {
+        position: relative;
+        overflow: hidden;
+    }
+
+    .bg-black.bg-opacity-50.p-4.pb-2.rounded-5.orange::before,
+    .bg-black.bg-opacity-50.p-4.pb-2.rounded-5.orange::after {
+        content: "";
+        position: absolute;
+        top: -2px;
+        left: -2px;
+        width: calc(100% + 4px);
+        height: calc(100% + 4px);
+        background: linear-gradient(120deg, #000000, rgba(0, 89, 255, 0.2), #000000);
+        z-index: -1;
+        background-size: 300% 300%;
+        animation: wave 25s linear infinite;
+    }
+
+    /* Keyframes for the wave animation */
+    @keyframes wave {
+        0% { background-position: -100% 50%; }
+        100% { background-position: 200% 50%; }
+    }
+</style>
