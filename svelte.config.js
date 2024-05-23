b22492911be245917950f882f1ab5d395ac21fd8
AnonymousGCA <gabrycaretti@gmail.com>
2024-05-23 17:13:48 +0200

    Changelogs: - Vercel adapter.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node'; // Depending on deployment target, you may need to change this another adapter
+import adapter from '@sveltejs/adapter-vercel'; // Depending on deployment target, you may need to change this another adapter
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +7,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			precompress: true,
 		})
 	}
 };
